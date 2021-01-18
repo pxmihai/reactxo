48bf4a99acd6885ee1e6714e875f8197561466a1
pxmihai <pxmihai@yahoo.com>
2021-01-18 11:43:25 +0200

    - shake, stir

--- src/App.js
+++ src/App.js
@@ -6,10 +6,16 @@ import _ from "lodash";
 function App() {
 
     const [xoState, setXoState] = useState([
-        ["", "", "", ""],
-        ["", "", "", ""],
-        ["", "", "", ""],
-        ["", "", "", ""],
+        ["", "", "", "","","","","","",""],
+        ["", "", "", "","","","","","",""],
+        ["", "", "", "","","","","","",""],
+        ["", "", "", "","","","","","",""],
+        ["", "", "", "","","","","","",""],
+        ["", "", "", "","","","","","",""],
+        ["", "", "", "","","","","","",""],
+        ["", "", "", "","","","","","",""],
+        ["", "", "", "","","","","","",""],
+        ["", "", "", "","","","","","",""]
     ]);
     const currentPlayer = useRef("X");
 
@@ -22,46 +28,76 @@ function App() {
             else currentPlayer.current = "X";
             setXoState(newXoState);
 
-            if (hasWin("X", newXoState)) alert("A castigat X!")
-            if (hasWin("O", newXoState)) alert("A castigat O!")
+            if (hasWin("X", newXoState)) alert("X has won!")
+            if (hasWin("O", newXoState)) alert("O has won!")
 
         }
 
     };
 
     const hasWin = (player, xoState) => {
-        //liniile
-        for (let row = 0; row < 4; ++row) {
+        // lines
+        for (let row = 0; row < 10; ++row) { // change 1
             if (xoState[row][0] === player &&
                 xoState[row][1] === player &&
                 xoState[row][2] === player &&
-                xoState[row][3] === player
+                xoState[row][3] === player && //change 2
+                xoState[row][4] === player &&
+                xoState[row][5] === player &&
+                xoState[row][6] === player &&
+                xoState[row][7] === player &&
+                xoState[row][8] === player &&
+                xoState[row][9] === player &&
+                xoState[row][10]
             )
                 return true;
         }
 
-        //coloanele
-        for (let column = 0; column < 4; ++column) {
+        // columns
+        for (let column = 0; column < 10; ++column) { //change 3
             if (xoState[0][column] === player &&
                 xoState[1][column] === player &&
                 xoState[2][column] === player &&
-                xoState[3][column] === player
+                xoState[3][column] === player &&
+                xoState[4][column] === player && // change 4
+                xoState[5][column] === player &&
+                xoState[6][column] === player &&
+                xoState[7][column] === player &&
+                xoState[8][column] === player &&
+                xoState[9][column] === player &&
+                xoState[10][column]
             )
                 return true;
         }
-        //    diagonalele, principala, secundara
+        //    diagonale principala, secundara
         if (
             xoState[0][0] === player &&
             xoState[1][1] === player &&
             xoState[2][2] === player &&
-            xoState[3][3] === player
+            xoState[3][3] === player && //change 5
+            xoState[4][4] === player &&
+            xoState[5][6] === player &&
+            xoState[6][6] === player &&
+            xoState[7][7] === player &&
+            xoState[8][8] === player &&
+            xoState[9][9] === player &&
+            xoState[10][10] === player
+
         )
             return true;
 
-        if (xoState[0][3] === player &&
-            xoState[2][2] === player &&
-            xoState[1][1] === player &&
-            xoState[3][0] === player)
+        if (xoState[0][10] === player &&
+            xoState[1][9] === player &&
+            xoState[2][8] === player &&
+            xoState[3][7] === player &&
+            xoState[4][6] === player &&
+            xoState[5][5] === player &&
+            xoState[6][4] === player &&
+            xoState[7][3] === player &&
+            xoState[8][2] === player &&
+            xoState[9][1] === player &&
+            xoState[10][0] === player
+        )
             return true;
 
         return false;
